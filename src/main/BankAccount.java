c96a0d4af049bcb7287ec1d84544243c19284f1d
evanfroehlich <evanfroehlich99@gmail.com>
2026-03-17 14:16:34 -0500

    starting close account code

--- src/main/BankAccount.java
+++ src/main/BankAccount.java
@@ -3,17 +3,27 @@ package main;
 public class BankAccount {
 
     private double balance;
+    private boolean active;
 
     public BankAccount() {
         this.balance = 0;
+        this.active = true;
     }
 
     public void deposit(double amount) {
-        if(amount > 0) {
+        if(amount > 0 && active) {
             this.balance += amount;
         } else {
             throw new IllegalArgumentException();
         }
+    }
+
+    public void closeAccount() {
+        this.active = false;
+    }
+
+    public boolean getAccountStatus() {
+        return this.active;
     }
 
     public double getBalance() {
--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class MainMenu {
 
-    private static final int EXIT_SELECTION = 2;
-	private static final int MAX_SELECTION = 2;
+    private static final int EXIT_SELECTION = 3;
+	private static final int MAX_SELECTION = 3;
 
 	private BankAccount userAccount;
     private Scanner keyboardInput;
@@ -19,7 +19,8 @@ public class MainMenu {
         System.out.println("Welcome to the 237 Bank App!");
         
         System.out.println("1. Make a deposit");
-        System.out.println("2. Exit the app");
+        System.out.println("2. Close an existing account");
+        System.out.println("3. Exit the app");
 
     }
 
@@ -36,6 +37,8 @@ public class MainMenu {
         switch (selection) {
             case 1:
                 performDeposit();
+            case 2:
+                closeAccount();
         }
     }
 
@@ -46,6 +49,10 @@ public class MainMenu {
             depositAmount = keyboardInput.nextInt();
         }
         userAccount.deposit(depositAmount);
+    }
+
+    public void closeAccount() {
+        userAccount.closeAccount();
     }
 
     public void run() {
